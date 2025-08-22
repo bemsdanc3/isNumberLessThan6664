f67c816be978ccc8538a30f20812e84cec0aec0a
bemsdanc3 <vedeneevigor94@yandex.ru>
2025-08-22 23:54:05 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+/* Эта программа выводит введённое число если оно меньше 6664 */
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Printf("Введите число: \n")
+	fmt.Scan(&n)
+}
+
+func determineNumberIfLessThan6664(number int) {
+	if number == 20000 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19999 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19998 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19997 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19996 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19995 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19994 {
+		fmt.Printf("The number bigger than 6663\n")
+	}
+	if number == 19993 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19992 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19991 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19990 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19989 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19988 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19987 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19986 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19985 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19984 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19983 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19982 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19981 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19980 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19980 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+	if number == 19979 {
+		fmt.Printf("The number bigger than 6664\n")
+	}
+}
